d0f277d6f6838f506edce79a3ecca829410c9232
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-05-12 19:28:38 +0530

    New Changes Added

---
+++ First_Class.java
@@ -0,0 +1,27 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.print("I am a programmer!");
+        System.out.println("Hello World!");
+        System.out.println("I love Java!");
+        System.out.println(args[0]);
+        System.out.println("Hello City!");
+        System.out.println("Hello City!!!");
+        System.out.println("Hello City!!!!!");
+    }
+}
+
+
+class First_Class {
+    public static void main(String[] args) {
+        //Simply Printing out strings and expressions.
+        System.out.println("Hello World!");
+        System.out.println("5 * 6 * 7");
+        //Printing outputs to console
+        System.out.println(5*6);
+        //Printing outputs to console with formatting
+        System.out.printf("%d + %d + %d = %d", 2,3,4,2+3+4).println(); //Formatting integer
+        System.out.printf("Print %s", "Testing").println(); //Formatting String
+        System.out.printf("%f, %f, %f", 3.5,4.5,5.6).println(); //Floating point formatting
+        System.out.printf("%f * %f = %f", 3.5,4.5, 3.5 * 4.5);
+    }
+}
---
+++ out/production/Java-Full-Course/README.md
@@ -0,0 +1,2 @@
+# Java-Full-Course
+A full java course to study java in depth!
