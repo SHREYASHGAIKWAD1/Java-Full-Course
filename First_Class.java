3d41ba74aefc7a83850999ec4abc6254ebfd1859
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-05-21 12:49:53 +0530

    Changes2

--- First_Class.java
+++ First_Class.java
@@ -10,6 +10,67 @@ class Main {
     }
 }
 
+//Data types
+//Primitive data types
+class data_types {
+    public static void main(String[] args) {
+        //Non-Numeric
+        char c = '=';
+        System.out.println(c);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        //Numeric
+        byte b = 2; //(range = -228 : 227  /8bits)
+        System.out.println(b);
+
+        short s = 5; //(range = -32768 : 32767  /16bits)
+        System.out.println(s);
+
+        int i = 10; //(range = -2,147,483,648 : 2,147,483,647  /32bits)
+        System.out.println(i);
+
+        long l = 20; //(range = -9,223,372,036,854,775,808 : 9,223,372,036,854,775,807  /64bits)
+        System.out.println(l);
+
+        long g = 3_000_000_000L;
+        System.out.println(g);
+
+        float f = 3.8F;  //(range = 1.40239846 x 10^-45 : 3.40239846 x 10^38  /32bits)
+        System.out.println(f);
+
+        double d = 2.7; //(range = 4.94065............ x 10^-324 : 1.79769............. x 10^308  /64bits)
+        System.out.println(d);
+    }
+}
+
+//Non-Primitive data types
+class string {
+    public static void main(String[] args) {
+        String s = "I love java!";
+        System.out.println(s);
+
+        System.out.println(s.charAt(7));
+        System.out.println(s.charAt(2));
+
+        String s2 = """
+                Harry potter and the philosopher's Stone
+                Harry potter and the Chambers of secrets
+                Harry potter and the prisoner of Azkaban
+                Harry potter and the Goblet of fire
+                Harry potter and the Order of phoenix
+                Harry potter and the half Blood prince
+                Harry potter and the Deathly hallows Part 1
+                Harry potter and the Deathly hallows Part 2
+                """;
+
+        System.out.println(s2);
+        /*
+        This is a multiline comment example!
+         */
+    }
+}
 
 class First_Class {
     public static void main(String[] args) {
@@ -29,5 +90,8 @@ class First_Class {
         //Variable Declaration and Assignment
         int b = 20; int c = 80;
         System.out.println(Math.max(b,c));
+        //If variable declared but not assigned, it will give out an error in a running program but in jshell
+        // it will assign the declared variable a value of 0 as the current value!
+        int declaredVariable;
     }
 }
