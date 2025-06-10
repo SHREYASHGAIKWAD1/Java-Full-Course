c45d168f76efe5747708876abfb044288a63f935
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-06-10 13:11:42 +0530

    TDIHIRG

--- 2. Conditionals_and_for_loop.java
+++ 2. Conditionals_and_for_loop.java
@@ -137,5 +137,12 @@ class methodsInJava {
             }
         }
 
+        //Multiple parameters in java method.
+        //ex:-
+        void sumOfNums(int num1, int num2) {
+            System.out.println(num1 + num2);
+        }
+
+
     }
 }
