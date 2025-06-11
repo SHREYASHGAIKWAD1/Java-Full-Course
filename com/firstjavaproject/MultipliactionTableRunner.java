21600bf20fd812bcaba0f13b2e87511a30d8b59a
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-06-12 01:03:18 +0530

    YDFWTNV

---
+++ com/firstjavaproject/MultipliactionTableRunner.java
@@ -0,0 +1,8 @@
+package com.firstjavaproject;
+
+public class MultipliactionTableRunner {
+    public static void main(String[] args) {
+        MultiplicationTable table = new MultiplicationTable();
+        table.print();
+    }
+}
---
+++ com/firstjavaproject/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package com.firstjavaproject;
+
+public class MultiplicationTable {
+    //Multiplication Table for 5
+
+    void print() {
+        for(int i = 1; i<=10; i++) {
+            System.out.printf("%d * %d = %d", 5, i, 5*i).println();
+        }
+    }
+}
