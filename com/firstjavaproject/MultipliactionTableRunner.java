945b28ca01de7a23ff33100d9a0d21d11cf57e8f
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-06-13 15:19:09 +0530

    TVUGUDY

--- com/firstjavaproject/MultipliactionTableRunner.java
+++ com/firstjavaproject/MultipliactionTableRunner.java
@@ -3,6 +3,8 @@ package com.firstjavaproject;
 public class MultipliactionTableRunner {
     public static void main(String[] args) {
         MultiplicationTable table = new MultiplicationTable();
-        table.print();
+        //table.print();
+        //table.print(8);
+        table.print(2, 11, 20);
     }
 }
--- com/firstjavaproject/MultiplicationTable.java
+++ com/firstjavaproject/MultiplicationTable.java
@@ -8,4 +8,16 @@ public class MultiplicationTable {
             System.out.printf("%d * %d = %d", 5, i, 5*i).println();
         }
     }
+
+    void print(int num) {
+        for(int i = 1; i<=10; i++) {
+            System.out.printf("%d * %d = %d", num, i, num*i).println();
+        }
+    }
+
+    void print(int num1, int from, int to) {
+        for(int i = from; i<=to; i++) {
+            System.out.printf("%d * %d = %d", num1, i, num1*i).println();
+        }
+    }
 }
