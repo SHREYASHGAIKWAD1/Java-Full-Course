a01434899d16cec55407eafc6232f0635ef95ef9
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-06-17 23:30:14 +0530

    TFMKLYB

---
+++ com/oops/Book.java
@@ -0,0 +1,31 @@
+package com.oops;
+
+public class Book {
+    //State
+    int noOfCopies;
+
+    //Behavior
+    void open() {
+        System.out.println("Opened the book.");
+    }
+    void read() {
+        System.out.println("Reading the book.");
+    }
+    void close() {
+        System.out.println("Book Closed!");
+    }
+
+    public static void main(String[] args) {
+        Book artOfComputerProgramming = new Book();
+        Book effectiveJava = new Book();
+        Book cleanCode = new Book();
+
+        artOfComputerProgramming.open();
+        artOfComputerProgramming.read();
+        artOfComputerProgramming.close();
+
+        artOfComputerProgramming.noOfCopies = 50;
+        effectiveJava.noOfCopies = 80;
+        cleanCode.noOfCopies = 70;
+    }
+}
