5fc5fadf9d7bda8a1601382f64c37ab0597d9698
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-06-18 12:56:28 +0530

    YTDWVWDNI

--- com/oops/MotorBike.java
+++ com/oops/MotorBike.java
@@ -1,6 +1,17 @@
 package com.oops;
 
 public class MotorBike {
+
+    //State
+    private int speed;
+
+    //Behavior
+    void setSpeed(int speed) {
+        this.speed = speed;
+        System.out.println(speed);
+        System.out.println(this.speed);
+    }
+
     void start() {
         System.out.println("Bike Started!");
     }
