4f0142fa016dfd5a9e761eaec3bd88fa5146e78c
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-07-07 19:53:52 +0530

    TEEWCd

---
+++ com/javaprimitivedatatypeindepth/SimpleInterestCalculator.java
@@ -0,0 +1,21 @@
+package com.javaprimitivedatatypeindepth;
+
+import java.math.BigDecimal;
+
+public class SimpleInterestCalculator {
+
+    BigDecimal principal;
+
+    BigDecimal interest;
+
+    public SimpleInterestCalculator(String principal, String interest) {
+        this.principal = new BigDecimal(principal);
+        this.interest = new BigDecimal(interest).divide(new BigDecimal(100));
+    }
+
+    public BigDecimal calculateTotalValue(int noOfYears) {
+        // totalValue = principal + principal * interest * noOfyears
+        BigDecimal totalValue = principal.add(principal.multiply(interest).multiply(new BigDecimal(noOfYears)));
+        return totalValue;
+    }
+}
