5d0efc091ab32bbcf6f72d27793b3b106f30dfb6
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-07-07 23:51:41 +0530

    YMLJ1D

--- com/javaprimitivedatatypeindepth/MyCharRunner.java
+++ com/javaprimitivedatatypeindepth/MyCharRunner.java
@@ -1,7 +1,13 @@
 package com.javaprimitivedatatypeindepth;
 
 public class MyCharRunner {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        MyChar myChar = new MyChar('c');
+        System.out.println(myChar.isVowel());
+        System.out.println(myChar.isNumber());
+        //System.out.println(myChar.isAlaphabet());
+        //myChar.printLowerCaseAlphabets();
+        //myChar.printUpperCaseAlphabets();
     }
 }
