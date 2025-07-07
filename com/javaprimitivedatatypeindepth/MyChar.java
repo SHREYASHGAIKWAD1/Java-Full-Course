25064782fcaad6a6bc0c3a9883209b797c6bafa0
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-07-07 23:50:23 +0530

    TAXSEWQ

--- com/javaprimitivedatatypeindepth/MyChar.java
+++ com/javaprimitivedatatypeindepth/MyChar.java
@@ -1,5 +1,33 @@
 package com.javaprimitivedatatypeindepth;
 
 public class MyChar {
+    //State
+    private char ch;
 
+    //behavior
+    public MyChar(char ch) {
+        this.ch = ch;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
+    public boolean isVowel() {
+        if(ch == 'a'|| ch == 'e'|| ch == 'i'
+                || ch == 'o'|| ch == 'u' || ch == 'A' || ch == 'E'
+                || ch == 'I' || ch == 'O' || ch == 'U') {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isNumber() {
+        if(ch == )
+            return null;
+    }
 }
