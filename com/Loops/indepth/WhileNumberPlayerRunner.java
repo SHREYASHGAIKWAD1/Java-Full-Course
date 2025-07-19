a4896b0ae43fb63bc444015c7804d9abd921d755
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-07-19 19:50:57 +0530

    GATWQRD

---
+++ com/Loops/indepth/WhileNumberPlayerRunner.java
@@ -0,0 +1,10 @@
+package com.Loops.indepth;
+
+public class WhileNumberPlayerRunner {
+
+    public static void main(String[] args) {
+        WhileNumberPlayer number = new WhileNumberPlayer(30);
+        number.printSquaresUptoLimit();
+        number.printCubesUptoLimit();
+    }
+}
