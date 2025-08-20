4e2f06e12da8669d4d7ee5b730e9ee399a075ec3
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2025-08-20 21:56:47 +0530

    JAGACXZ

--- com/ArrayandArrayList/Student.java
+++ com/ArrayandArrayList/Student.java
@@ -34,3 +34,63 @@ public class Student {
         return this.name;
     }
 
+    public int getNumberOfMarks() {
+        return this.marks.size();
+    }
+
+    public int getTotalSumOfMarks() {
+        int sum = 0;
+        for (int mark: marks) {
+            sum = sum + mark;
+        }
+        return sum;
+    }
+
+	/*public int getMaximumMarks() {
+		int maximum = Integer.MIN_VALUE;
+		for (int mark: marks) {
+			if (mark > maximum) {
+				maximum = mark;
+			}
+		}
+		return maximum;
+	}*/
+
+    public int getMaximumMarks() {
+        return Collections.max(this.marks);
+    }
+
+	/*public int getMinimumMarks() {
+		int minimum = Integer.MAX_VALUE;
+		for (int mark: marks) {
+			if (mark < minimum) {
+				minimum = mark;
+			}
+		}
+		return minimum;
+	}*/
+
+    public int getMinimumMarks() {
+        return Collections.min(this.marks);
+    }
+
+    public BigDecimal getAverageMarks() {
+        int sum = getTotalSumOfMarks();
+        int number = getNumberOfMarks();
+        BigDecimal average = new BigDecimal(sum).divide(new BigDecimal(number), 3, RoundingMode.UP);
+        return average;
+    }
+
+    public String toString() {
+        return name + marks;
+    }
+
+    public void addNewMark(int i) {
+        this.marks.add(i);
+    }
+
+    public void removeMarkAtIndex(int i) {
+        this.marks.remove(i);
+    }
+}
+
