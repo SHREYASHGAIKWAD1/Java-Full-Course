a2362c76f369ef2c71341891f01f8c5c283d9d99
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2026-01-23 03:27:52 +0530

    JNDCSRE

---
+++ com/JavaModularization2ServiceJar/sorting/utility/MySortingUtil.java
@@ -0,0 +1,13 @@
+package com.JavaModularization2ServiceJar.sorting.utility;
+
+import java.util.List;
+
+import com.Sorting.Algorithm.BubbleSort;
+
+public class MySortingUtil {
+	
+	public List<String> sort(List<String> names) {
+		BubbleSort bubbleSort = new BubbleSort();
+		return bubbleSort.sort(names);
+	}
+}
