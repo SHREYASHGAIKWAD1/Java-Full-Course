dd1ed24ac4654b529f59a539ed2986c7ae94ef38
Shreyash Gaikwad <shreyashgaikwad02367@gmail.com>
2026-01-23 03:25:31 +0530

    HGADVDT

---
+++ com/JavaModularization2ServiceJar/sorting/algorithm/BubbleSort.java
@@ -0,0 +1,10 @@
+package com.JavaModularization2ServiceJar.sorting.algorithm;
+
+import java.util.List;
+
+public class BubbleSort {
+
+	public List<String> sort(List<String> name) {
+		return name;
+	}
+}
